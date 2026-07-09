80c358cb22de15d59abccf3eb5ed8b59b445a9b9
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:06:24 +0530

    Add login page foundation

---
+++ app/login/page.tsx
@@ -0,0 +1,5 @@
+import Container from "@/components/layout/Container";
+
+export default function Login(){
+ return <main><Container><section className="py-20"><h1 className="text-5xl font-bold">Login</h1><div className="mt-8 border border-white/10 rounded-xl p-6">Authentication form will be integrated here.</div></section></Container></main>;
+}
