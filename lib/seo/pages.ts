cbbec60d963a63f788cf7869fa4a2496ce1dacf7
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:27:39 +0530

    Add page SEO configuration structure

---
+++ lib/seo/pages.ts
@@ -0,0 +1,4 @@
+export const seoPages={
+ home:{title:"AASHA by Deep Shikha",description:"Luxury fashion experience"},
+ products:{title:"AASHA Collections",description:"Explore premium collections"}
+};
