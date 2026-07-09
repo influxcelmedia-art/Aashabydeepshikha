aa5244feecd7279c6539b545ed9609b0993f3957
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:06:03 +0530

    Add checkout form foundation

---
+++ components/checkout/CheckoutForm.tsx
@@ -0,0 +1,3 @@
+export default function CheckoutForm(){
+ return <div className="border border-white/10 rounded-xl p-6"><h2 className="text-2xl">Checkout</h2><p className="mt-4 text-white/70">Shipping and payment details will be added here.</p></div>;
+}
