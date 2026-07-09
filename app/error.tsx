09af0facd128b72672223f131cdf57b66c767b0b
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:10:11 +0530

    Add global error boundary

---
+++ app/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Error(){
+ return <div className="min-h-screen flex items-center justify-center"><h1 className="text-3xl">Something went wrong. Please try again.</h1></div>;
+}
