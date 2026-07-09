93f53e03137156b2b857d897df208dfd51e74673
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:05:09 +0530

    Add cart summary component foundation

---
+++ components/cart/CartSummary.tsx
@@ -0,0 +1,3 @@
+export default function CartSummary(){
+ return <div className="border border-white/10 rounded-xl p-6"><h2 className="text-xl">Cart Summary</h2><p className="mt-4 text-white/70">Your selected products will appear here.</p></div>;
+}
