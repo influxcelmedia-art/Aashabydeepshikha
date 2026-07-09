3de9bed2fc8406ab1a23668701bdd2c09096a2f0
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:06:29 +0530

    Add registration page foundation

---
+++ app/register/page.tsx
@@ -0,0 +1,5 @@
+import Container from "@/components/layout/Container";
+
+export default function Register(){
+ return <main><Container><section className="py-20"><h1 className="text-5xl font-bold">Create Account</h1><div className="mt-8 border border-white/10 rounded-xl p-6">Registration form will be integrated here.</div></section></Container></main>;
+}
