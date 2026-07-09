046c346e6c07380a400ec9599e5fb4dbc9eabc93
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:10:51 +0530

    Add newsletter section foundation

---
+++ components/home/Newsletter.tsx
@@ -0,0 +1,3 @@
+export default function Newsletter(){
+ return <section className="py-16 text-center"><h2 className="text-3xl">Join AASHA</h2><p className="mt-4 text-white/70">Receive updates and new collection launches.</p></section>;
+}
