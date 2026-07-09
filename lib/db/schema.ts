44b5f3c4905328405f2090354809b781794731d4
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:06:58 +0530

    Add database model schema foundation

---
+++ lib/db/schema.ts
@@ -0,0 +1,5 @@
+export const models={
+ User:{fields:["id","name","email","password"]},
+ Product:{fields:["id","name","price","image","category"]},
+ Order:{fields:["id","userId","items","status","total"]}
+};
