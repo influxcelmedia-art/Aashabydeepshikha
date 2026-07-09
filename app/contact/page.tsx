a2529e3392499da8a05e62069750e81db4008e27
influxcelmedia-art <influxcelmedia@gmail.com>
2026-07-09 16:05:02 +0530

    Add contact page foundation

---
+++ app/contact/page.tsx
@@ -0,0 +1,5 @@
+import Container from "@/components/layout/Container";
+
+export default function Contact(){
+ return <main><Container><section className="py-20"><h1 className="text-5xl font-bold">Contact AASHA</h1><p className="mt-6 text-white/70">We would love to hear from you.</p></section></Container></main>;
+}
